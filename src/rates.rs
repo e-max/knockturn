e1046114776efdb29c4b6c3482d3069516866d0c
hashmap <hashmap@gmail.com>
2019-02-20 14:37:53 +0100

    again

--- src/rates.rs
+++ src/rates.rs
@@ -3,6 +3,7 @@ use crate::errors::*;
 use actix::prelude::*;
 use actix_web::client;
 use actix_web::HttpMessage;
+use futures;
 use futures::future::Future;
 use log::{debug, error, info};
 use serde::Deserialize;
@@ -26,14 +27,50 @@ impl Actor for RatesFetcher {
 pub struct FetchRates {}
 
 impl Message for FetchRates {
-    type Result = Box<Future<Item = (), Error = Error>>; //Result<(), Error>;
+    type Result = Result<(), Error>;
 }
 
 impl Handler<FetchRates> for RatesFetcher {
-    type Result = Box<Future<Item = (), Error = Error>>; // Result<(), Error>;
+    type Result = Result<(), Error>; // Box<Future<Item = (), Error = Error>>; // ;
 
     fn handle(&mut self, msg: FetchRates, _: &mut Self::Context) -> Self::Result {
-        Box::new(client::get(
+        println!("fetching");
+        Ok(())
+        //client::get(
+        //    "https://api.coingecko.com/api/v3/simple/price?ids=grin&vs_currencies=btc%2Cusd%2Ceur",
+        //)
+        //.header("User-Agent", "Actix-web")
+        //.header("Accept", "application/json")
+        //.finish()
+        //.unwrap()
+        //.send()
+        //.map_err(|e| Error::Fetch(format!("failed to fetch exchange rates: {:?}", e)))
+        //.and_then(|response| {
+        //    response
+        //        .body()
+        //        .map_err(|e| Error::Fetch(format!("Payload error: {:?}", e)))
+        //        .and_then(move |body| Ok(str::from_utf8(&body)?))
+        //        //.from_err(Error::Fetch(format!("failed to parse body")))
+        //        .and_then(|str| Ok(serde_json::from_str::<Rates>(&str)?))
+        //        .map_err(|e| Error::Fetch(format!("failed to parse json: {:?}", e)))
+        //        .and_then(|rates| {
+        //            self.db
+        //                .send(RegisterRate { rates: rates.grin })
+        //                .map_err(|_| Error::Fetch(format!("failed to parse body")))
+        //                .and_then(|db_response| db_response)
+        //                .from_err()
+        //                .and_then(|_| Ok(()))
+        //        })
+        //})
+    }
+}
+impl RatesFetcher {
+    pub fn new(db: Addr<DbExecutor>) -> Self {
+        RatesFetcher { db }
+    }
+
+    fn fetch(&mut self) -> impl Future<Item = (), Error = Error> {
+        client::get(
             "https://api.coingecko.com/api/v3/simple/price?ids=grin&vs_currencies=btc%2Cusd%2Ceur",
         )
         .header("User-Agent", "Actix-web")
@@ -58,11 +95,6 @@ impl Handler<FetchRates> for RatesFetcher {
                         .from_err()
                         .and_then(|_| Ok(()))
                 })
-        }))
-    }
-}
-impl RatesFetcher {
-    pub fn new(db: Addr<DbExecutor>) -> Self {
-        RatesFetcher { db }
+        })
     }
 }
