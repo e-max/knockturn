8717b5591110f716fed50b3bd58116f5033668e7
Max Lavrenov <max.lavrenov@gmail.com>
2019-03-13 17:06:40 +0100

    use different colors for transactions

--- src/filters.rs
+++ src/filters.rs
@@ -1,4 +1,5 @@
 use crate::models::Money;
+use crate::models::{Transaction, TransactionStatus, TransactionType};
 use askama::Error;
 use chrono::NaiveDateTime;
 pub fn grin(nanogrins: &i64) -> Result<String, Error> {
@@ -7,4 +8,13 @@ pub fn grin(nanogrins: &i64) -> Result<String, Error> {
 
 pub fn pretty_date(date: &NaiveDateTime) -> Result<String, Error> {
     Ok(date.format("%d.%m.%Y %H:%M:%S").to_string())
+}
+
+pub fn transaction_color(tx: &Transaction) -> Result<&'static str, Error> {
+    Ok(match (tx.transaction_type, tx.status) {
+        (TransactionType::Payout, TransactionStatus::Confirmed) => "success",
+        (TransactionType::Payout, TransactionStatus::Pending) => "info",
+        (TransactionType::Payment, TransactionStatus::Rejected) => "secondary",
+        (_, _) => "light",
+    })
 }
