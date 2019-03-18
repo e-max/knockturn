0755a3a8160f946d1dd38a8962b44d3e914a4c04
Max Lavrenov <max.lavrenov@gmail.com>
2019-03-18 22:55:44 +0100

    some optimisation on qr code

--- src/qrcode.rs
+++ src/qrcode.rs
@@ -2,11 +2,11 @@ use crate::errors::Error;
 use consistenttime::ct_u8_slice_eq;
 use image::png::PNGEncoder;
 use image::{Luma, Pixel};
-use qrcode::QrCode;
+use qrcode::{EcLevel, QrCode};
 
 pub fn as_png(s: &str) -> Result<Vec<u8>, Error> {
-    let qrcode = QrCode::new(s).map_err(|e| Error::General(s!(e)))?;
-
+    let qrcode =
+        QrCode::with_error_correction_level(s, EcLevel::L).map_err(|e| Error::General(s!(e)))?;
     let png = qrcode.render::<Luma<u8>>().build();
     let mut buf: Vec<u8> = Vec::new();
     PNGEncoder::new(&mut buf)
