64da418405aa17f67c62d1e12f7d06cae6b4d425
Max Lavrenov <max.lavrenov@gmail.com>
2019-03-20 22:15:05 +0100

    reduce size of QR code

--- src/qrcode.rs
+++ src/qrcode.rs
@@ -7,7 +7,7 @@ use qrcode::{EcLevel, QrCode};
 pub fn as_png(s: &str) -> Result<Vec<u8>, Error> {
     let qrcode =
         QrCode::with_error_correction_level(s, EcLevel::L).map_err(|e| Error::General(s!(e)))?;
-    let png = qrcode.render::<Luma<u8>>().build();
+    let png = qrcode.render::<Luma<u8>>().module_dimensions(4, 4).build();
     let mut buf: Vec<u8> = Vec::new();
     PNGEncoder::new(&mut buf)
         .encode(&png, png.width(), png.height(), Luma::<u8>::color_type())
